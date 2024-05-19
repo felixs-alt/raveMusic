0c3c9e3abb2c4b6cde64926af962fa438163a9aa
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2024-05-19 13:23:02 +0200

    t

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
-const app = express();
 const ytdlp = require('ytdlp-nodejs')
 const {createWriteStream} = require('fs')
 
+const app = express();
+const crypto = require('crypto');
 app.use(express.json());
-
+app.use(express.static('files'))
 app.listen(3000, () => {
     console.log(`Server Started at ${3000}`)
 });
 
-app.post(“/ytwav”, (request, response) => {
+app.post("/ytwav", (request, response) => {
+    let uuid = crypto.randomUUID();
+    const file = createWriteStream("files/"+uuid+".wav")
     const url = request.body.url;
-    ytdlp.stream(url: url,{filter: "extractaudio",quality: 10, format: "wav"}):stream
-    response.
+    const stream = ytdlp.stream(url,{filter: "audioonly", quality: 10, format: "wav"}).pipe(file);
+    response.send(uuid+".wav");
  });
